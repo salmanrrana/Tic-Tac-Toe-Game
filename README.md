e3cc81b210fa825548a3c7c2e5c34630bc915518
Salman Rana <salman.r.rana@gmail.com>
2017-03-21 10:12:04 -0500

    Point functionality set up. Still needs work but happy I got it to this points

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
+
+Flexbox and jQuery
+used to help design my game
+
+
 # game
 this is my first game. Enjoy!
+
+Fonts Used
 
 font-family: 'Monofett', cursive;
 font-family: 'Codystar', cursive;
 font-family: 'Bungee Inline', cursive;
+
+
+read some of my notes throughout my code to get some insignt into he javascript
--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@ alert('sanity check');
 
 //sets the counter number to 1 in order to track future clicks
 var counter = 1
-var i = 0;
-var j = 0;
 
 $(document).ready(function() {
   //when a row is clicked, add the pattern. looks at function below called patternForCurrentPlayer and
   // within the patternForCurrentPlayer it also tracks clicks
   $('.row').on('click', patternForCurrentPlayer);
 })
+
 
 //when a click happens track clicks. looks at function below called clickCounter
 function clickCounter() {
@@ -32,8 +31,8 @@ function patternForCurrentPlayer() {
       $('.messages').html("Cheater! Cheater! Pumpkin Eater!").fadeOut(4000);
       // alert("Cheater! Cheater! Pumpkin Eater!");
     } else if ($(this).hasClass('marked2')) {
-      $('.messages').html("This is already clicked dummy!").fadeOut(4000);
-      // alert("This is already clicked dummy!");
+      // $('.messages').html("This is already clicked dummy!").fadeOut(4000);
+      alert("This is already clicked dummy!");
     } else {
       $(this).addClass('marked1').append('<h3>X</h3>')
       //now it adds the click counter if it does add the class
@@ -45,15 +44,16 @@ function patternForCurrentPlayer() {
   //if it is divisible by 2 then put player2
   else {
     if ($(this).hasClass('marked1')) {
-      $('.messages').html("Cheater! Cheater! Pumpkin Eater!").fadeOut(4000);
-      // alert("Cheater! Cheater! Pumpkin Eater!");
+      // $('.messages').html("Cheater! Cheater! Pumpkin Eater!").fadeOut(4000);
+      alert("Cheater! Cheater! Pumpkin Eater!");
     } else if ($(this).hasClass('marked2')) {
-      $('.messages').html("This is already clicked dummy!").fadeOut(4000);
-      // alert("this is already clicked dummy!");
+      // $('.messages').html("This is already clicked dummy!").fadeOut(4000);
+      alert("this is already clicked dummy!");
     } else {
       $(this).addClass('marked2').append('<h3>O</h3>');
       //now it adds the click counter if it does add the class
       clickCounter();
+      //it will also look for a winner after each turn
       lookForWinner();
       addPoint();
     }
@@ -82,56 +82,98 @@ $(function() {
 });
 
 
-
+//Looks for different winning possibilities
 function lookForWinner() {
   // player 1 accross wins
   if ($('.row1').hasClass('marked1') && $('.row2').hasClass('marked1') && $('.row3').hasClass('marked1')) {
+    var score1= $('.scorebox1').html();
+    score1++
+    $('.scorebox1').html(score1);
     alert('Player 1 kicked butt');
   } else if ($('.row4').hasClass('marked1') && $('.row5').hasClass('marked1') && $('.row6').hasClass('marked1')) {
-    alert("Player 1 is master of all");
+    var score1= $('.scorebox1').html();
+    score1++
+    $('.scorebox1').html(score1);alert("Player 1 is master of all");
   } else if ($('.row7').hasClass('marked1') && $('.row8').hasClass('marked1') && $('.row9').hasClass('marked1')) {
+    var score1= $('.scorebox1').html();
+    score1++
+    $('.scorebox1').html(score1);
     alert("Player 1 will succeed in life");
     // player 1 diagonal wins
   } else if ($('.row1').hasClass('marked1') && $('.row5').hasClass('marked1') && $('.row9').hasClass('marked1')) {
+    var score1= $('.scorebox1').html();
+    score1++
+    $('.scorebox1').html(score1);
     alert("Player 2 must now buy lunch for Player 1 for the rest of the year");
   } else if ($('.row3').hasClass('marked1') && $('.row5').hasClass('marked1') && $('.row7').hasClass('marked1')) {
+    var score1= $('.scorebox1').html();
+    score1++
+    $('.scorebox1').html(score1);
     alert("Ding Dang Player 1...You Rule!!!");
     //player 1 down wins
   } else if ($('.row1').hasClass('marked1') && $('.row4').hasClass('marked1') && $('.row7').hasClass('marked1')) {
+    var score1= $('.scorebox1').html();
+    score1++
+    $('.scorebox1').html(score1);
     alert("Player 1 will have nothing but luck in life");
   } else if ($('.row2').hasClass('marked1') && $('.row5').hasClass('marked1') && $('.row8').hasClass('marked1')) {
+    var score1= $('.scorebox1').html();
+    score1++
+    $('.scorebox1').html(score1);
     alert("Go buy Player 1 a new pair of socks, Player 2");
   } else if ($('.row3').hasClass('marked1') && $('.row6').hasClass('marked1') && $('.row9').hasClass('marked1')) {
+    var score1= $('.scorebox1').html();
+    score1++
+    $('.scorebox1').html(score1);
     alert("Hey! Good Job Player 1. Im gonna start calling you Jeff");
   }
   // player 2 across wins
   else if ($('.row1').hasClass('marked2') && $('.row2').hasClass('marked2') && $('.row3').hasClass('marked2')) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert('Player 2 kicked butt');
   } else if ($('.row4').hasClass('marked2') && $('.row5').hasClass('marked2') && $('.row6').hasClass('marked2')) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert("Player 2 is master of all");
   } else if ($('.row7').hasClass('marked2') && $('.row8').hasClass('marked2') && $('.row9').hasClass('marked2')) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert("Player 1 must now buy lunch for Player 2 for the rest of the year");
     // diagonal wins
   } else if ($('.row1').hasClass('marked2') && $('.row5').hasClass('marked2') && $('.row9').hasClass('marked2')) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert("Hey! Good Job Player 1. Im gonna start calling you Julia");
   } else if ($('.row3').hasClass('marked2') && $('.row5').hasClass('marked2') && $('.row7').hasClass('marked2')) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert("Ding Dang Player 2...You Rule!!!");
     //down wins
   } else if ($('.row1').hasClass('marked2') && $('.row4').hasClass('marked2') && $('.row7').hasClass('marked2')) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert("Go buy Player 2 a new pair of socks, Player 1. Now!!");
   } else if ($('.row2').hasClass('marked2') && $('.row5').hasClass('marked2') && $('.row8').hasClass('marked2')) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert("player 2 wins");
   } else if ($('.row3').hasClass('marked2') && $('.row6').hasClass('marked2') && $('.row9').hasClass('marked2')) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert("Player 2 will have nothing but luck in life");
   } else if (counter >= 10) {
+    var score2= $('.scorebox2').html();
+    score2++
+    $('.scorebox2').html(score2);
     alert('We need a winner. PLAY AGAIN!');
   }
 };
-
-function addPoint() {
-  if (lookForWinner === 'marked1') {
-    $('.scorebox1').html(i++);
-  } else if (lookForWinner === 'marked2'){
-    $('.scorebox2').html(j++);
-  }
-}
